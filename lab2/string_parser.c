ffd686090477df89792089afcf10a7be96b880bf
flamboh <oliver.boorstein@gmail.com>
2026-04-17 13:44:04 -0700

    lab2 + gitignore

--- lab2/string_parser.c
+++ lab2/string_parser.c
@@ -1,53 +1,74 @@
-/*
- * string_parser.c
- *
- *  Created on: Nov 25, 2020
- *      Author: gguan, Monil
- *
- */
-
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "string_parser.h"
 
 #define _GUN_SOURCE
 
+char *ltrim(char *s)
+{
+    while(isspace(*s)) s++;
+    return s;
+}
+
+
+char *rtrim(char *s)
+{
+    char* back = s + strlen(s);
+    while(isspace(*--back));
+    *(back+1) = '\0';
+    return s;
+}
+
+
+char *trim(char *s)
+{
+    return rtrim(ltrim(s));
+}
+
 int count_token (char* buf, const char* delim)
 {
-	//TODO：
-	/*
-	*	#1.	Check for NULL string
-	*	#2.	iterate through string counting tokens
-	*		Cases to watchout for
-	*			a.	string start with delimeter
-	*			b. 	string end with delimeter
-	*			c.	account NULL for the last token
-	*	#3. return the number of token (note not number of delimeter)
-	*/
+  if (buf == NULL) return 0;
+  int len = strlen(buf);
+  int result = 0;
+
+
+  for (int i = 0; i < len; ++i) {
+    if (i == 0 && buf[i] == *delim) continue;
+    if (i == len - 1 && buf[i] == *delim) break;
+    if (buf[i] == *delim) result++;
+    if (i == len - 1) result++;
+  }
+  return result;
 }
 
 command_line str_filler (char* buf, const char* delim)
 {
-	//TODO：
-	/*
-	*	#1.	create command_line variable to be filled and returned
-	*	#2.	count the number of tokens with count_token function, set num_token. 
-    *           one can use strtok_r to remove the \n at the end of the line.
-	*	#3. malloc memory for token array inside command_line variable
-	*			based on the number of tokens.
-	*	#4.	use function strtok_r to find out the tokens 
-    *   #5. malloc each index of the array with the length of tokens,
-	*			fill command_list array with tokens, and fill last spot with NULL.
-	*	#6. return the variable.
-	*/
+  command_line result;
+  result.command_list = NULL;
+  result.num_token = count_token(buf, delim);
+  result.command_list = malloc((result.num_token + 1) * sizeof(char *));
+  char *token;
+  char *save = buf;
+  int cur = 0;
+
+  while ((token = strtok_r(save, delim, &save))) {
+    token = trim(token);
+    if (strlen(token) < 1) continue;
+    result.command_list[cur] = malloc(strlen(token) + 1);
+    strcpy(result.command_list[cur], token);
+    cur++;
+  }
+
+  result.command_list[cur] = NULL;
+  return result;
 }
 
 
 void free_command_line(command_line* command)
 {
-	//TODO：
-	/*
-	*	#1.	free the array base num_token
-	*/
+  for (int i = 0; i < command->num_token; ++i) {
+    free(command->command_list[i]);
+  }
+  free(command->command_list);
 }
